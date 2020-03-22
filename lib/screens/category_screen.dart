8748334bed739c2dbf536b7331d3d75b0732bb3c
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-03-23 00:02:51 +0500

    Screens

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,50 @@
+import 'package:budget_ui/models/category_model.dart';
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatefulWidget {
+  final Category category;
+  CategoryScreen({this.category});
+  @override
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.category.name),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+            iconSize: 30.0,
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(20.0),
+              height: 250.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 2),
+                    blurRadius: 6.0,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
