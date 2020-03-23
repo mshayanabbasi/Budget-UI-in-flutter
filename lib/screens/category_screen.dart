88d08b440c313abb88f6a8feda6730b6a74c7fba
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-03-23 12:45:10 +0500

    Updates

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,4 +1,7 @@
+import 'package:budget_ui/helpers/color_helper.dart';
 import 'package:budget_ui/models/category_model.dart';
+import 'package:budget_ui/models/expense_model.dart';
+import 'package:budget_ui/widgets/radial_painter.dart';
 import 'package:flutter/material.dart';
 
 class CategoryScreen extends StatefulWidget {
@@ -11,6 +14,13 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpent = 0;
+    widget.category.expenses.forEach((Expense expense) {
+      totalAmountSpent += expense.cost;
+    });
+    final double amountLeft = widget.category.maxAmount - totalAmountSpent;
+    final double percent = amountLeft / widget.category.maxAmount;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category.name),
@@ -40,6 +50,23 @@ class _CategoryScreenState extends State<CategoryScreen> {
                     blurRadius: 6.0,
                   ),
                 ],
+              ),
+              child: CustomPaint(
+                foregroundPainter: RadialPainter(
+                  bgColor: Colors.grey,
+                  lineColor: getColor(context, percent),
+                  percent: percent,
+                  width: 15.0,
+                ),
+                child: Center(
+                  child: Text(
+                    '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount}',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
