601fb233dcbde465ea6b3d40b6fa20d6eae6edf7
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-03-23 13:02:20 +0500

    End

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -12,6 +12,60 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  _buildExpenses() {
+    List<Widget> expenesList = [];
+    widget.category.expenses.forEach((Expense expense) {
+      expenesList.add(
+        Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.symmetric(
+            horizontal: 20.0,
+            vertical: 10.0,
+          ),
+          height: 80.0,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0, 2),
+                blurRadius: 6.0,
+              ),
+            ],
+          ),
+          child:Padding(
+            padding: EdgeInsets.all(30.0),
+            child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                expense.name,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '-\$${expense.cost.toStringAsFixed(2)}',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+          ), 
+        ),
+      );
+    });
+    return Column(
+      children: expenesList,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double totalAmountSpent = 0;
@@ -69,6 +123,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
                 ),
               ),
             ),
+            _buildExpenses(),
           ],
         ),
       ),
